340752d7bd05177d6c2e3080fc9dd8be6b3546fe
Usanas <sabinerugugula@gmail.com>
2019-04-05 13:04:42 +0200

    changes2

---
+++ UI/JS/adminStaffInterface.js
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Admin/Staff Interface</title>
+<style>
+body{
+	background-color: OldLace;
+}
+ .diva {
+	background-color: LightGray;
+	width: 33.3%;
+	height:100%;
+	
+	text-align: center;
+	float: left;
+	}
+h1{
+	color: SaddleBrown;
+}
+h3{
+	color: SaddleBrown;
+}
+ hr {
+	color: ;
+	background-color: SaddleBrown;
+	width: 100%;
+	height: 10px;
+	border-radius: 5px;
+
+}
+ a{
+	text-decoration: none;
+}SaddleBrown;
+}
+</style>
+</head>
+<body>
+	<div>
+<h1>Nanase Bank</h1>
+<hr>
+</div>
+<div class="diva">
+
+<a href = "C:\Users\IDL\Documents\Nanase Bank\Deleting An Account" target = "_self"><h3>Deleting An Account</h3></a>
+</div>
+<div class ="diva">
+<a href = "C:\Users\IDL\Documents\Nanase Bank\Account Record" target = "_self"><h3>View Bank Account Record</h3></a>
+</div>
+<div class ="diva">
+<a href = "C:\Users\IDL\Documents\Nanase Bank\Account lists" target = "_self"><h3>View lists Of All Bank Accounts</h3></a>
+</div>
+</div>
+</body>
+</html>
---
+++ UI/JS/adminStaffUser.js
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body{
+	background-color: OldLace;
+}
+ .diva {
+	background-color: LightGray;
+	width: 20%;
+	height:100%;
+	
+	text-align: center;
+	float: left;
+	}
+h1{
+	color: SaddleBrown;
+}
+h3{
+	color: SaddleBrown;
+}
+ hr {
+	color: ;
+	background-color: SaddleBrown;
+	width: 100%;
+	height: 10px;
+	border-radius: 5px;
+
+}
+ a{
+	text-decoration: none;
+}
+</style>
+<title>Admin/Staff/User</title>
+</head>
+<body>
+	<div>
+<h1>Nanase Bank</h1>
+<hr>
+</div>
+<div class = "diva">
+
+
+</div>
+</body>
+</html>
