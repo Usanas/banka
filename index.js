713dd843efc53be5acb4c444de508261aaf3d79e
Usanas <sabinerugugula@gmail.com>
2019-04-14 12:48:29 +0200

    npm dependencies and creating first APIs endpoints

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const index = express();
+
+
+
+
+module.exports = index;
+  
---
+++ server.js
@@ -0,0 +1,6 @@
+const http = require('http');
+const index = require('../banka/index');
+
+const port = process.env.PORT || 3000;
+const server = http.createServer(index);
+server.listen(port);
