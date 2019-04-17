92f5665a4e68d86c34fb1c2bc95db271027009ab
Usanas <sabinerugugula@gmail.com>
2019-04-17 14:52:16 +0200

    troubleshoot heroku problem

--- Servers/index.js
+++ Servers/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const index = express();
-
+index.use((req, rest,next) => {
+    res.status(200).json({
+        message: 'it works!'
+    });
+});
 
 module.exports = index;
+  
 
--- Servers/server.js
+++ Servers/server.js
@@ -1,7 +1,6 @@
 const http = require('http');
-const index = require('../banka/index');
+const index = require('index');
 
 const port = process.env.PORT || 3000;
 const server = http.createServer(index);
-
 server.listen(port);
